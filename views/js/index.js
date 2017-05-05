3249660fda4ebeb7095ed3515a91d077b9d9f4ca
shenjiajun <shenjj@2345.com>
2017-05-05 18:02:04 +0800

    add vue

--- views/js/index.js
+++ views/js/index.js
@@ -2,10 +2,20 @@
  * Created by shenjj on 2017/5/4.
  */
 import Vue from "vue";
+// import jouery from "jquery";
+// import bootstrap from "bootstrap";
+
 
 let app = new Vue({
     el: '#app',
     data: {
+        show: true,
         message: 'Hello Vue!'
+    },
+    methods: {
+        toggleShow: function () {
+            this.show = !this.show;
+            this.message = this.message.split('').reverse().join('');
+        }
     }
 });

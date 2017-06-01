c5a7382e617a4f3888a4b61da12348a702b909fb
shenjiajun <shenjiajun53@qq.com>
2017-06-01 16:33:12 +0800

    面包屑

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,7 @@ module.exports = {
         index: path.resolve(__dirname + '/views/js/index.js'),
         signin: path.resolve(__dirname + '/views/js/signin.js'),
         manage_header: path.resolve(__dirname + '/views/js/manage_header.js'),
+        movie_list: path.resolve(__dirname + '/views/js/movie_list.js'),
         side_bar: path.resolve(__dirname + '/views/js/side_bar.js'),
         VueComponents: path.resolve(__dirname + '/views/component/VueComponents.js'),
         UrlUtil: path.resolve(__dirname + '/views/utils/UrlUtil.js'),

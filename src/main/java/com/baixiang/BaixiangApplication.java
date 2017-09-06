2833cfca02e342e1896c42424d9bc8551da47819
shenjiajun <shenjiajun53@qq.com>
2017-09-06 23:01:19 +0800

    打包

--- src/main/java/com/baixiang/BaixiangApplication.java
+++ src/main/java/com/baixiang/BaixiangApplication.java
@@ -1,8 +1,11 @@
 package com.baixiang;
 
+import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
 
 @SpringBootApplication
@@ -14,3 +17,18 @@ public class BaixiangApplication {
 		SpringApplication.run(BaixiangApplication.class, args);
 	}
 }
+
+
+//public class BaixiangApplication extends SpringBootServletInitializer {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(applicationClass, args);
+//	}
+//
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//		return application.sources(applicationClass);
+//	}
+//
+//	private static Class<Application> applicationClass = Application.class;
+//}

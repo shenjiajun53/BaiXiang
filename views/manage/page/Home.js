7d6e2135a8b098a25a3a96e6ed7ab5de92ccd499
shenjiajun <shenjiajun53@qq.com>
2017-11-02 22:59:10 +0800

    table

--- views/manage/page/Home.js
+++ views/manage/page/Home.js
@@ -8,32 +8,7 @@ import {Card, Pagination, Table} from 'antd';
 import Urls from "../../utils/Urls"
 
 // injectTapEventPlugin();
-const columns = [
-    {
-        title: 'Id',
-        dataIndex: 'id',
-        key: 'id',
-        width: 150,
-    },
-    {
-        title: 'Name',
-        dataIndex: 'movieName',
-        key: 'movieName',
-        width: 150,
-    },
-    {
-        title: 'movieInfo',
-        dataIndex: 'movieInfo',
-        key: 'movieInfo',
-        width: 150,
-    },
-    {
-        title: 'imdbUrl',
-        dataIndex: 'imdbUrl',
-        key: 'imdbUrl',
-        width: 150,
-    }
-];
+
 
 class Home extends Component {
     constructor(props) {
@@ -77,8 +52,7 @@ class Home extends Component {
     }
 
     render() {
-        let movieListView;
-
+        // let movieListView;
         // if (null !== this.state.movieList) {
         //     movieListView = this.state.movieList.map(
         //         (movieInfo) => {
@@ -100,20 +74,118 @@ class Home extends Component {
         //         }
         //     );
         // }
+        // movieListView =
+        //     <div style={{
+        //         display: "flex",
+        //         flexDirection: "row",
+        //         flexWrap: "wrap",
+        //         marginTop: 16,
+        //         marginLeft: 32,
+        //         marginBottom: 16,
+        //         marginRight: 32
+        //     }}>
+        //         {movieListView}
+        //     </div>;
+
+        const columns = [
+            {
+                title: 'Id',
+                dataIndex: 'id',
+                key: 'id',
+                width: 50,
+            },
+            {
+                title: '片名',
+                dataIndex: 'movieName',
+                key: 'movieName',
+                width: 250,
+            },
+            {
+                title: '简介',
+                dataIndex: 'movieInfo',
+                key: 'movieInfo',
+                width: 700,
+            },
+            {
+                title: '豆瓣链接',
+                dataIndex: 'doubanUrl',
+                key: 'doubanUrl',
+                width: 250,
+            }
+        ];
+
         return (
-            <div>
-                <div style={{
-                    display: "flex",
-                    flexDirection: "row",
-                    flexWrap: "wrap",
-                    marginTop: 16,
-                    marginLeft: 32,
-                    marginBottom: 16,
-                    marginRight: 32
-                }}>
-                    {movieListView}
-                </div>
-                <Table columns={columns} dataSource={this.state.movieList}/>
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+                marginTop: 16,
+                marginLeft: 32,
+                marginBottom: 16,
+                marginRight: 32,
+                alignItems: "center"
+            }}>
+                <Table
+                    rowKey={(item) => item.id}
+                    pagination={false}
+                    // columns={columns}
+                    dataSource={this.state.movieList}>
+                    <Table.Column
+                        title="ID"
+                        dataIndex="id"
+                        key="id"
+                        width={50}
+                        render={(text, record) =>
+                            (
+                                <div>
+                                    <a href={Urls.MANAGE_EDIT_MOVIE + record.id}>{text}</a>
+                                </div>
+                            )}
+                    />
+                    <Table.Column
+                        title="片名"
+                        dataIndex="movieName"
+                        key="movieName"
+                        width={250}
+                        render={(text, record) =>
+                            <div>
+                                <a href={Urls.MANAGE_EDIT_MOVIE + record.id}>{text}</a>
+                            </div>}
+                    />
+                    <Table.Column
+                        title="海报"
+                        dataIndex="posterUrl"
+                        key="posterUrl"
+                        width={250}
+                        render={(text, record) =>
+                            <div>
+                                <img style={{height: 100}}
+                                     src={text}/>
+                            </div>}
+                    />
+                    <Table.Column
+                        title="简介"
+                        dataIndex="movieInfo"
+                        key="movieInfo"
+                        width={700}
+                        render={(text, record) => {
+                            let content = text;
+                            if (null !== text && text.length > 200) {
+                                content = text.substr(0, 200) + "...";
+                            }
+                            return (<div style={{height: 100}}>{content}</div>);
+                        }}
+                    />
+                    <Table.Column
+                        title="豆瓣链接"
+                        dataIndex="doubanUrl"
+                        key="doubanUrl"
+                        width={300}
+                        render={(text, record) =>
+                            <div>
+                                <a href={text}>{text}</a>
+                            </div>}
+                    />
+                </Table>
                 <Pagination showQuickJumper={true}
                             pageSize={this.state.pageSize}
                             total={this.state.total}
